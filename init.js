75915920f6f4ee1c77cf850980f73feef86cd2d8
Helbak <helbak77@gmail.com>
2019-06-02 10:22:27 +0300

    Added functions of remove and reDraw and Tests

--- init.js
+++ init.js
@@ -1,49 +1,85 @@
 const tree = new myBSTree();
 let field = document.getElementById('field');
 let stringHTML = "";
-let el=0;
-let stringStyle = "";
+let el = 0;
+
 const number = document.getElementById('add_number');
-const screen = document.getElementById('field');
+const deleteNumber = document.getElementById("delete_number");
 
 function upgrade() {
     tree.upgrade();
     stringHTML = "";
-
     field.innerHTML = stringHTML;
 }
 
 function createNode() {
     el = parseInt(number.value);
-    number.value =null;
-    const node = tree.push(tree.rootNode, el);
-    drawStringHTML(node);
+    number.value = null;
+    if(tree.isThisNodeInArray(el)===false) {
+        const node = tree.push(tree.rootNode, el);
+        drawStringHTML(node);
+    }
+};
 
-
+function createNodeFromArray(ara) {
+    for(let k=0; k<ara.length; k++) {
+        console.log("  function createNodeFromArray "+ ara[k]);
+        const node = tree.push(tree.rootNode, ara[k]);
+        // drawStringHTML(node);
+        // console.log(" after function createNodeFromArray "+ ara[k]);
+       setTimeout(drawStringHTML(node), 5000);
+    }
 };
 
 function stringID(n) {
     return "id" + n;
 }
+
 function toScreen() {
     let alarm = document.getElementById("text");
     alarm.innerHTML = "<h1> out of screen</h1> ";
-}
+};
+
 function drawStringHTML(node) {
-    if(node.posLeft>780 || node.posLeft<20||node.posTop>580){
+    if (node.posLeft > 780 || node.posLeft < 20 || node.posTop > 580) {
         toScreen();
         return;
     }
     if (tree.size == 0) {
         stringHTML = stringHTML + '<div class="node" id=root>' + tree.rootNode.el + '</div>';
+       tree.sumSize();
+        field.innerHTML = stringHTML;
+        return tree.rootNode;
     }
     if (tree.size >= 1) {
-        stringHTML = stringHTML + '<div class="node" id=' + stringID(node.el) + '>' + node.el + '</div>';
+        let newDiv = document.createElement('div');
+        newDiv.className = "node";
+        newDiv.id = stringID(node.el);
+        newDiv.innerHTML = node.el;
+        let br = document.createElement('br');
+        field.appendChild(br);
+        field.appendChild(newDiv);
+        setStyle(node);
     }
-    field.innerHTML = stringHTML;
+    // field.innerHTML = stringHTML;
+
     setStyle(node);
 };
-
+// function drawStringHTML(node) {
+//     if(node.posLeft>780 || node.posLeft<20||node.posTop>580){
+//         toScreen();
+//         return;
+//     }
+//     if (tree.size == 0) {
+//         stringHTML = stringHTML + '<div class="node" id=root>' + tree.rootNode.el + '</div>';
+//     }
+//     if (tree.size >= 1) {
+//         stringHTML = stringHTML + '<div class="node" id=' + stringID(node.el) + '>' + node.el + '</div>';
+//     }
+//     field.innerHTML = stringHTML;
+//
+//     setStyle(node);
+// };
 function setStyle(node) {
     let nextNode = document.getElementById(stringID(node.el));
     nextNode.style = "position: absolute; top:" + node.posTop + "px; left: " + node.posLeft + "px;"
@@ -51,9 +87,43 @@ function setStyle(node) {
 };
 
 function myBSTree() {
-
+    this.sizeArray = 0;
+    this.nodesArray = [];
     this.rootNode = null;
     this.size = 0;
+    this.isThisNodeInArray=function (el) {
+        for(let i =0; i<this.nodesArray.length; i++){
+            if(el===this.nodesArray[i]){
+                return true;
+            }
+        }
+        return false;
+    };
+    this.addNodesToArray = function (el) {
+        if(this.isThisNodeInArray(el)===false){
+        this.nodesArray[this.sizeArray] = parseInt(el);
+        this.sizeArray++;
+        }
+    };
+    this.deleteNodeFromArray = function () {
+        let el = parseInt(deleteNumber.value);
+        deleteNumber.value = "";
+        let numOfElement;
+        for (let i = 0; i < this.nodesArray.length; i++) {
+            if (this.nodesArray[i] === el) {
+                numOfElement = i;
+            }
+            if (numOfElement === null) {
+                return;
+            }
+        }
+        this.nodesArray.splice(numOfElement, 1);
+        upgrade();
+        this.reDraw();
+    };
+    this.reDraw = function () {
+        createNodeFromArray(this.nodesArray);
+    };
     this.getPositionTop = function (el, firstNode, posTop) {
 
         if (firstNode.el > el && firstNode.left === null) {
@@ -63,10 +133,10 @@ function myBSTree() {
             return posTop;
         }
         if (firstNode.el < el && firstNode.right !== null) {
-            return this.getPositionTop(el, firstNode.right, posTop + 30);
+            return this.getPositionTop(el, firstNode.right, posTop + 50);
         }
         if (firstNode.el > el && firstNode.left !== null) {
-            return this.getPositionTop(el, firstNode.left, posTop + 30);
+            return this.getPositionTop(el, firstNode.left, posTop + 50);
         }
     };
     this.getPositionLeft = function (el, firstNode, posLeft) {
@@ -93,7 +163,7 @@ function myBSTree() {
         }
     };
     this.getFloor = function (posTop) {
-        return (posTop - 10) / 30;
+        return (posTop -10)/30;
     };
     this.getStep = function (floor) {
         let step = 404;
@@ -125,11 +195,12 @@ function myBSTree() {
             return false;
         }
         if (this.rootNode == null) {
-            this.rootNode = this.createNode(el, 1, 405, 40);
+            this.rootNode = this.createNode(parseInt(el), 1, 405, 40);
             return this.rootNode;
         }
     };
     this.createNode = function (el, floor, posLeft, posTop) {
+        this.addNodesToArray(el);
         return {
             el: el,
             floor: floor,
@@ -158,7 +229,8 @@ function myBSTree() {
         if (firstNode.el < el && firstNode.right === null) {
             let posTop = this.getPositionTop(el, this.rootNode, 70);
             let floor = this.getFloor(posTop);
-            let posLeft = this.getPositionLeft(el, this.rootNode, 404);;
+            let posLeft = this.getPositionLeft(el, this.rootNode, 404);
+            ;
             firstNode.right = this.createNode(el, floor, posLeft, posTop);
             this.sumSize();
             return firstNode.right;
